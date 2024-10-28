6abd1ab3dcbc459cf578f260290460c9d0599652
cyh666 <2609922564@qq.com>
2024-10-29 04:11:32 +0800

    README提交

--- blockmesh.sh
+++ blockmesh.sh
@@ -7,60 +7,92 @@ BLUE='\033[0;34m'
 NONEC='\033[0m'
 
 # 检查是否有curl，如果没有安装则安装
-if ! command -v curl &> /dev/null; then
-    sudo apt update
-    sudo apt install curl -y
-fi
-sleep 2
+#if ! command -v curl &> /dev/null; then
+#    sudo apt update
+#    sudo apt install curl -y
+#fi
+#sleep 2
 
 
 # 检查bc
-echo -e "${BLUE}正在检查您的操作系统版本...${NONEC}"
-if ! command -v bc &> /dev/null; then
-    sudo apt update
-    sudo apt install bc -y
-fi
-sleep 1
+#echo -e "${BLUE}正在检查您的操作系统版本...${NONEC}"
+#if ! command -v bc &> /dev/null; then
+#    sudo apt update
+#    sudo apt install bc -y
+#fi
+#sleep 1
+#
+## 检查linux Ubuntu系统版本
+#UBUNTU_VERSION=$(lsb_release -rs)
+#REQUIRED_VERSION=22.04
+#
+#
+#if (( $(echo "UBUNTU_VERSION < $REQUIRED_VERSION" | bc -l) )); then
+#    echo -e "${RED}此节点需要最低版本的 Ubuntu 22.04 ${NONEC}"
+#    exit 1
+#fi
 
-# 检查linux Ubuntu系统版本
-ubuntu_version=$(lsb_release -rs)
-required_version=22.04
+
+function main_start() {
+    # 菜单
+     echo -e "${YELLOW}选择操作:${NONEC}"
+     echo -e "${BLUE}1)安装节点${NONEC}"
+     echo -e "${BLUE}2)查看日志${NONEC}"
+     echo -e "${RED}3)删除节点${NONEC}"
+     echo -e "${BLUE}4)退出${NONEC}"
+
+     read choice
+    
+      case $choice in
+          1)
+              install_node
+              ;;
+          2)
+              # 查看日志
+              cat_logs
+              ;;
+          3)
+              remove_node
+              ;;
+          4)
+              exit 0
+              ;;
+      esac
+}
 
 
-if (( $(echo "ubuntu_version < $required_version" | bc -l) )); then
-    echo -e "${RED}此节点需要最低版本的 Ubuntu 22.04 ${NONEC}"
-    exit 1
-fi
+function install_before() {
+      # 检查是否有curl，如果没有安装则安装
+      if ! command -v curl &> /dev/null; then
+          sudo apt update
+          sudo apt install curl -y
+      fi
+      sleep 1
+      # 检查bc
+      echo -e "${BLUE}正在检查您的操作系统版本...${NONEC}"
+      if ! command -v bc &> /dev/null; then
+          sudo apt update
+          sudo apt install bc -y
+      fi
+      sleep 1
+
+      # 检查linux Ubuntu系统版本
+      UBUNTU_VERSION=$(lsb_release -rs)
+      REQUIRED_VERSION=22.04
 
 
+      if (( $(echo "UBUNTU_VERSION < $REQUIRED_VERSION" | bc -l) )); then
+          echo -e "${RED}此节点需要最低版本的 Ubuntu 22.04 ${NONEC}"
+          exit 1
+      fi
 
-# 菜单
-echo -e "${YELLOW}选择操作:${NONEC}"
-echo -e "${BLUE}1)安装节点${NONEC}"
-echo -e "${BLUE}2)查看日志${NONEC}"
-echo -e "${RED}3)删除节点${NONEC}"
-echo -e "${BLUE}4)退出${NONEC}"
-
-read choice
-
-case $choice in
-    1)
-        install_node
-        ;;
-    2)
-        # 查看日志
-        cat_logs
-        ;;
-    3)
-        remove_node
-        ;;
-    4)
-        exit 0
-        ;;
-esac
+    
+}
 
 # 安装节点函数
 function install_node() {
+    #安装之前检查
+     install_before
      echo -e "${BLUE}安装 BlockMesh 节点${NONEC}"
         # 检查是否有tar命令
         if ! command -v tar &> /dev/null; then
@@ -137,3 +169,6 @@ function cat_logs() {
     sudo journalctl -u blockmesh.service -f
     echo "按 Ctrl+C 退出日志查看。"
 }
+
+# 主程序开始
+main_start
